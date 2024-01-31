481c165d891625339d4b5bf62803047cdea5052d
Steelerforreel <reelnicholas@gmail.com>
2024-01-30 22:43:30 -0500

    Adjusted server

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # Read-Rinse-Repeat
+  ![License](https://img.shields.io/badge/license-MIT-green.svg)
+  ## Welcome to an easy to use book searching application. With this application you will be able to search for any book you can think of and add it to a list to save and purchase in the future!
+
+  ## Table of Contents
+  
+  * [Installation Guide](#install)
+  
+  * [Usage](#usage)
+
+  * [Contributors](#contributors)
+
+  * [Test of Application](#testing)
+
+  * [Questions](#questions)
+
+  * [License](#license-📛)
+
+  ## Installation Guide
+  
+  How to install this application within the command prompt.
+
+  ```
+  npm i
+  ```
+
+
+  ## Usage
+  npm run start:develop
+
+  ## License 📛 
+
+    Copyright @ MIT. All rights reserved.
+
+    Licensed under the MIT license.
+
+  ## Contributors
+
+  I would like to give credit to
+
+  Jose Lopez tutoring at EDX @ OSU
+
+  ## Test of Application
+
+  Please follow this link to better understand the use for this application.
+
+  ```
+  
+  ```
+  
+  ## Questions?
+
+  Please contact me via email for any questions.
+
+  GitHub: [Steelerforreel](https://github.com/Steelerforreel/)
+
+  Email: reelnicholas@gmail.com
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -13,34 +14,28 @@ const server = new ApolloServer({
   resolvers,
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 const startApolloServer = async () => {
   await server.start();
 
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  
-  if (process.env.NODE_ENV === 'production') {
-      app.use(express.static(path.join(__dirname, '../client/build')));
-    }
-    
-    app.use(routes);
-    
+
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
+
+    if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
-  
-  app.use('/graphql', expressMiddleware(server));
 
   db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   });
+};
 
 startApolloServer();
 
